788f61b325441a066d95d2c0d0fc6788d2ab6fc1
Maksym <m.domaretsky@gmail.com>
2018-01-06 19:38:06 +0200

    add cars in main.js

--- js/main.js
+++ js/main.js
@@ -52,11 +52,25 @@ $(document).ready(function() {
     });
 
     const cars = [
-        { val: 0, text: 'One', img: '<img id="car" src="./images/ibiza.jpg" />', price: "20$" },
-        { val: 1, text: 'Two', img: '<img id="car" src="./images/aygo.jpg" />', price: "30$" },
-        { val: 2, text: 'Three', img: '<img id="car" src="./images/207.jpg" />', price: "40" },
-        { val: 3, text: 'four', img: '<img id="car" src="./images/for-two.jpg" />' },
-        { val: 4, text: 'five', img: '<img id="car" src="./images/micra.jpg" />' }
+        { val: 0, text: 'Smart ForTwo', img: '<img id="car" src="./images/for-two.jpg" />', price: "20$" },
+        { val: 1, text: 'Smart ForFour', img: '<img id="car" src="./images/for-four.png" />', price: "30$" },
+        { val: 2, text: 'Toyota Aygo', img: '<img id="car" src="./images/aygo.jpg" />', price: "40" },
+        { val: 3, text: 'Peugeot 107', img: '<img id="car" src="./images/107.jpg" />' },
+        { val: 4, text: 'Nissan Micra', img: '<img id="car" src="./images/micra.jpg" />' },
+        { val: 5, text: 'Volkswagen Up', img: '<img id="car" src="./images/up.jpg" />' },
+        { val: 6, text: 'Citroen C1', img: '<img id="car" src="./images/citroen.png" />' },
+        { val: 7, text: 'Peugeot 207', img: '<img id="car" src="./images/207.jpg" />' },
+        { val: 8, text: 'Citroen C3', img: '<img id="car" src="./images/c3.png" />' },
+        { val: 9, text: 'Peugeot 207', img: '<img id="car" src="./images/207.jpg" />' },
+        { val: 10, text: 'Opel Corsa', img: '<img id="car" src="./images/corsa.png" />' },
+        { val: 11, text: 'Ford Fiesta', img: '<img id="car" src="./images/fiesta.png" />' },
+        { val: 12, text: 'Seat Ibiza', img: '<img id="car" src="./images/ibiza.png" />' },
+        { val: 13, text: 'Toyota Yaris 207', img: '<img id="car" src="./images/yaris.png" />' },
+        { val: 14, text: 'Skoda Fabia', img: '<img id="car" src="./images/fabia.jpg" />' },
+        { val: 15, text: 'Skoda Fabia Universal', img: '<img id="car" src="./images/fabia2.jpg" />' },
+        { val: 16, text: 'Seat Ibiza Universal', img: '<img id="car" src="./images/ibiza2.jpg" />' },
+        { val: 17, text: 'Smart Electric', img: '<img id="car" src="./images/smart_electric.png" />' },
+        { val: 18, text: 'Volkswagen Up Elecric', img: '<img id="car" src="./images/electric-up.png" />' }
     ];
 
     const sel = $('<select>').appendTo('.select-car');
